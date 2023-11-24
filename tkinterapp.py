308cf3bedc36d6c136360a91a5901c2a123e12e3
snorretik <niels.magendans@gmail.com>
2023-11-24 20:15:42 +0100

    Intergrated Matplotlib into Tkinter
    Succesfully intergrated Matplotlib plot into Tkinter window.

--- tkinterapp.py
+++ tkinterapp.py
@@ -1,25 +1,36 @@
-import tkinter
-import sys
-from PIL import ImageTk, Image
+import pandas
+import matplotlib.pyplot as plt
+from createplot import mergingData
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from tkinter import *
+# from PIL import ImageTk, Image
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Plotting the plot!")
-window.geometry("750x500") 
+window.geometry("750x600") 
 
 def displayplot():
-    # Load the image
-    global photo
-    image = Image.open("plot.png")
-    image.thumbnail([450, sys.maxsize], Image.LANCZOS)
-    photo = ImageTk.PhotoImage(image)
+    # importing DataFrame
+    df = mergingData()
 
-    # Label widget to display the image
-    label = tkinter.Label(window, image=photo)
-    
-    label.pack(side="top")
+    # creating plotconditions
+    fig, ax = plt.subplots()
+    ax.plot(df['Date'], df['Close_x'])
+    ax.plot(df['Date'], df['Close_y'])
+
+    # create element with which to display plot in Tkinter
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side="top")
+
+    # create a toolbar to go with the plot
+    toolbar = NavigationToolbar2Tk(canvas, window)
+    toolbar.update()
+    canvas.get_tk_widget().pack()
 
 def setbutton():
-    button = tkinter.Button(window, width=10, height=2, text='plot', command=displayplot)
+    button = Button(window, width=10, height=2, text='plot', command=displayplot)
     button.pack(side="bottom")
 
 # Add button to screen
