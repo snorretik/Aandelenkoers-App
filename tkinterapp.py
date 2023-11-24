bfc9961a11148c85cab0b6b191ba2222cb2fd0a8
snorretik <niels.magendans@gmail.com>
2023-11-24 10:30:06 +0100

    created main window functionality
    Tkinter has a button that plots the image on click.

--- tkinterapp.py
+++ tkinterapp.py
@@ -1,21 +1,27 @@
 import tkinter
 from PIL import ImageTk, Image
 
-def displayfunction():
-    # Create the main window
-    window = tkinter.Tk()
-    window.title("Plotting the plot!")
-    window.geometry("750x500")    
-    
+window = tkinter.Tk()
+window.title("Plotting the plot!")
+window.geometry("750x500") 
+
+def displayplot():
     # Load the image
+    global photo
     image = Image.open("plot.png")
     photo = ImageTk.PhotoImage(image)
 
     # Label widget to display the image
     label = tkinter.Label(window, image=photo)
-    label.pack()
     
-    window.mainloop()
+    label.pack(side="top")
+
+def setbutton():
+    button = tkinter.Button(window, width=10, height=2, text='plot', command=displayplot)
+    button.pack(side="bottom")
+
+# Add button to screen
+setbutton()
 
 # Run the application
-displayfunction()
+window.mainloop()
