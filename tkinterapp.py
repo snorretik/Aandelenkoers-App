06ffd04cbf2094e9ea39f5a15d807007889cb4cc
snorretik <niels.magendans@gmail.com>
2023-11-26 16:34:20 +0100

    Small additions to GUI
    Experimenting with making room for some more buttons and label etc and placing them.

--- tkinterapp.py
+++ tkinterapp.py
@@ -8,11 +8,14 @@ from tkinter import *
 
 window = Tk()
 window.title("Plotting the plot!")
-window.geometry("750x600") 
+window.geometry("740x655") 
 
 def displayplot():
     # importing DataFrame
     df = mergingData()
+
+    topframe = Frame(window)
+    topframe.pack(side="top")
 
     # creating plotconditions
     fig, ax = plt.subplots()
@@ -20,21 +23,43 @@ def displayplot():
     ax.plot(df['Date'], df['Close_y'])
 
     # create element with which to display plot in Tkinter
-    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas = FigureCanvasTkAgg(fig, master=topframe)
     canvas.draw()
-    canvas.get_tk_widget().pack(side="top")
-
-    # create a toolbar to go with the plot
-    toolbar = NavigationToolbar2Tk(canvas, window)
-    toolbar.update()
     canvas.get_tk_widget().pack()
 
-def setbutton():
-    button = Button(window, width=10, height=2, text='plot', command=displayplot)
+    # create a toolbar to go with the plot
+    toolbar = NavigationToolbar2Tk(canvas, topframe)
+    toolbar.update()
+    canvas.get_tk_widget().pack(side="top")
+
+def setupgui():
+    bottomframe = Frame(window)
+    bottomframe.pack(side="bottom")
+
+    middleframe = Frame(window)
+    middleframe.pack(side="bottom")
+
+    emptylabel1 = Label(bottomframe, text="", height=2)
+    emptylabel1.pack(side="bottom")
+
+    button = Button(bottomframe, width=10, height=2, text='plot', command=displayplot)
     button.pack(side="bottom")
 
+    emptylabel2 = Label(bottomframe, text="---", height=2)
+    emptylabel2.pack(side="top")
+
+    labelbalance = Label(middleframe, text="20.000", font=38)
+    labelbalance.pack(side="left")
+
+    buttonbuy = Button(middleframe, text="buy order")
+    buttonbuy.pack(side="left")
+
+    buttonsell = Button(middleframe, text="sell order")
+    buttonsell.pack(side="left")
+
+
 # Add button to screen
-setbutton()
+setupgui()
 
 # Run the application
 window.mainloop()
